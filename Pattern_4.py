4867e6363ce10aea6815edcc64e2f45a0314784a
harshnion <86431354+harshnion@users.noreply.github.com>
2024-01-13 17:05:37 +0530

    Create Pattern_4.py

---
+++ Pattern_4.py
@@ -0,0 +1,30 @@
+n=int(input())
+inc=(n+1)//2
+dec=n-inc
+i=1
+while i<=inc:
+    q=1
+    while q<=i-1:
+        print(" ",end="")
+        q=q+1
+
+    j=1
+    while j<=i:
+        print("* ",end="")
+        j=j+1
+    print()
+    i=i+1
+i=1
+while i<=dec:
+    q=1
+    while q<= dec-i:
+        print(" ",end="")
+        q=q+1
+
+
+    j=1
+    while j<=dec-i+1:
+        print("* ",end="")
+        j=j+1
+    print()
+    i=i+1
